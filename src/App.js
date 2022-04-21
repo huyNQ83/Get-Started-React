8d95d88f834cbf9018b701d1db742e9970881c1b
Nguyen Quang Huy <nguyenquanghuy@Nguyens-MacBook-Pro.local>
2022-04-21 20:48:07 +0700

    Todo app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [ todos, setTodos ] = useState([]);
+  console.log("🚀 ~ file: App.js ~ line 7 ~ App ~ todos", todos)
+
+  const getTodo = () => {
+    const todoList = document.querySelector('#todoList').value
+    console.log("🚀 ~ file: App.js ~ line 11 ~ getTodo ~ todoList", todoList)
+
+    setTodos([...todos, todoList]);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className ='App'>
+      <h1>What needs to be done?</h1>
+      <input type="text" id='todoList'></input>
+      <button onClick={getTodo}>Add #{todos.length + 1}</button>
+
+      <ul>
+        { todos.map((todo, ids) =>(
+          <li key={ids}>{todo}</li>
+        ))}
+      </ul>
     </div>
+    
   );
 }
 
